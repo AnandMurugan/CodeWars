05fe5a15d89bdb5cd2da9f8bf471e0e4e627e0b5
Ananda Murugan <m.anandsp@hotmail.com>
2017-09-11 21:48:49 +0200

    Validate phone number

---
+++ KataPrograms/Kata.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace KataPrograms
+{
+    public static class Kata
+    {
+        public static bool ValidPhoneNumber(string phoneNumber)
+        {
+            return Regex.Match(phoneNumber,
+                @"^\([0-9]{3}\) [0-9]{3}\-[0-9]{4}$").Success;
+        }
+    }
+}
---
+++ KataProgramsTests/KataTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace KataPrograms.Tests
+{
+    public class KataTests
+    {
+        [Fact()]
+        public void ValidPhoneNumberTest()
+        {
+            Assert.True(Kata.ValidPhoneNumber("(123) 456-7890"));
+            Assert.False(Kata.ValidPhoneNumber("ABC(123) 456-7890test"));
+            Assert.False(Kata.ValidPhoneNumber("(111) 5X5-2345"));
+            Assert.True(Kata.ValidPhoneNumber("(010) 515-2345"));
+
+            Assert.False(Kata.ValidPhoneNumber("(123)456-7890"));
+            Assert.False(Kata.ValidPhoneNumber("(12) 456-7890"));
+            Assert.False(Kata.ValidPhoneNumber("(123) 45-7890"));
+            Assert.False(Kata.ValidPhoneNumber("(123) 456-789"));
+            Assert.False(Kata.ValidPhoneNumber("(123)  456-7890"));
+            Assert.False(Kata.ValidPhoneNumber("(123) 456--7890"));
+
+            Assert.False(Kata.ValidPhoneNumber("123) 456-7890"));
+            Assert.False(Kata.ValidPhoneNumber("(123 456-7890"));
+            Assert.False(Kata.ValidPhoneNumber("(123) 4567890"));
+
+            Assert.False(Kata.ValidPhoneNumber("(123)   456-7890"));
+        }
+    }
+}
