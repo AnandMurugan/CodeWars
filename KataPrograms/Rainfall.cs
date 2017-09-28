fb34ebcd2e6379ccd7e53acaee3a9dce5ed40dd8
anamur <m.anandsp@gmail.com>
2017-09-28 17:04:44 +0200

    Rainfall Kata

---
+++ KataPrograms/Rainfall.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace KataPrograms
+{
+    public class Record
+    {
+        public string Month { get; set; }
+
+        public double Rainfallcm { get; set; }
+    }
+
+    public class Rainfall
+    {
+        private static readonly string[] Alltowns =
+        {
+            "Rome", "London", "Paris", "NY", "Vancouver", "Sydney", "Bangkok", "Tokyo", "Beijing", "Lima", "Montevideo",
+            "Caracas", "Madrid", "Berlin", "Lon"
+        };
+
+        private static Dictionary<string, List<Record>> _townsDictionary;
+
+        private static Dictionary<string, List<Record>> GetTowns()
+        {
+            return _townsDictionary ?? new Dictionary<string, List<Record>>();
+        }
+
+        public static double Mean(string townName, string strng)
+        {
+            if (string.IsNullOrEmpty(strng))
+                return -1;
+
+            if (!Alltowns.Contains(townName))
+                return -1;
+
+            Parse(strng);
+            var townRecords = GetTownRecords(townName);
+            var sum = townRecords.Sum(townRecord => townRecord.Rainfallcm);
+            var avg = sum / townRecords.Count;
+
+            return avg;
+        }
+
+        public static double Variance(string townName, string strng)
+        {
+            if (string.IsNullOrEmpty(strng))
+                return -1;
+
+            if (!Alltowns.Contains(townName))
+                return -1;
+
+            Parse(strng);
+            var townRecords = GetTownRecords(townName);
+            var sum = townRecords.Sum(townRecord => townRecord.Rainfallcm);
+            var avg = sum / townRecords.Count();
+            var variance = townRecords.Sum(townRecord => (townRecord.Rainfallcm - avg) * (townRecord.Rainfallcm - avg));
+
+            return variance / townRecords.Count;
+        }
+
+        private static List<Record> GetTownRecords(string townName)
+        {
+            var existing = new List<Record>();
+            if (GetTowns().ContainsKey(townName))
+                existing = _townsDictionary[townName];
+            return existing;
+        }
+
+        public static void Parse(string strng)
+        {
+            var townvalues = strng.Split('\n');
+            foreach (var townvalue in townvalues)
+            {
+                var rainfallvalues = townvalue.Split(':')[1].Split(',');
+                var rainfallRecords = rainfallvalues.Select(rainfallvalue => new Record
+                    {
+                        Month = rainfallvalue.Split(' ')[0],
+                        Rainfallcm = Convert.ToDouble(rainfallvalue.Split(' ')[1], CultureInfo.InvariantCulture)
+                    })
+                    .ToList();
+
+                GetTowns().Add(townvalue.Split(':')[0], rainfallRecords);
+            }
+        }
+    }
+}
